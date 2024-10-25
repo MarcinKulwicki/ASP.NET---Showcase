0349df4e55855129b3aa7452f54474cd69a0b33e
marcin kulwicki <MarcinKulwicki@protonmail.com>
2024-10-25 11:40:39 +0200

    starting point

---
+++ Web App - Showcase/WeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Web_App___Showcase
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+    }
+}
